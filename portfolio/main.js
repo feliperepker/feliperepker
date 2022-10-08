ed8630f0730802bd1a2ff064d4e2994a50475b78
Felipe Repker <102967559+feliperepker@users.noreply.github.com>
2022-10-07 21:44:43 -0300

    Add tilting

--- portfolio/main.js
+++ portfolio/main.js
@@ -9,3 +9,13 @@ menuToggle.addEventListener('click', () =>{
     menuSection.classList.toggle('on', show)
     show = !show;
 })
+
+
+VanillaTilt.init(document.querySelector(".container-img"), {
+    max: 10,
+    speed: 200,
+    reverse: true,
+    glare: true,
+    "max-glare": 0.1,
+    perspective: 2000,
+  });
