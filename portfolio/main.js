2f2ef3bb549a34bc2f26b1e46b6b144725bae627
Felipe Repker <102967559+feliperepker@users.noreply.github.com>
2022-10-11 11:20:31 -0300

    adding skills logics to mobile

--- portfolio/main.js
+++ portfolio/main.js
@@ -32,29 +32,89 @@ const observer = new IntersectionObserver(entries => {
   Array.from(document.querySelectorAll('.init-hidden')).forEach(element =>{
     observer.observe(element)
   })
-  
-
-var test =  Array.from(document.querySelectorAll('.skills-card'))
+var anotherCounter = 0;
+var counter = 0;
+var oldCard;
+var containerCards = document.querySelector('.container-skills')
+var project = document.querySelector('.transition-text')
+var arrayCards =  Array.from(document.querySelectorAll('.skills-card'))
 var textSkills = document.querySelector('#skills-text')
- test.forEach(card => {
+
+
+var x = window.matchMedia("(max-width: 1000px)")
+
+arrayCards.forEach(card => {
   card.addEventListener('click', () => {
-   if (card == test[0]){
-    textSkills.innerHTML = "HTML was the first skill that I learned, I used in a lot of my projects, in one project expecific a learn a lot of HTML in how to create a form. It's not easy to learn about DOM elements and how to integrate them with JS."
-   }
-   if(card == test[1]){
-    textSkills.innerHTML = "I love CSS so much, It's facinating what we can do with CSS. I try to do a lot of animations and things that I don't use before to learn more and more about CSS."
-   }
-   if(card == test[2]){
-    textSkills.innerHTML = "I used a lot of SCSS in my projects, it's easiear to create variables with SCSS and to interact with DOM elements. It's so good how you can keep your files organized with SCSS."
-   }
-   if(card == test[3]){
-    textSkills.innerHTML = "React is new for me, I learned a little in a recent project and I'm trying to use more in everything I do."
-   }
-   if(card == test[4]){
-    textSkills.innerHTML = "I'm learning Back-end Python in EBAC, using Django, Ngnix, Gunicorn and integrate this in a Virtual Box machine using Linux."
-   }
-   if(card == test[5]){
-    textSkills.innerHTML = "Part of my course in EBAC teach's SQL to create a database, relations and use the back-end to interact with the data source."
-   }
+    project.style.opacity = 0;
+    oldCard != card && counter == 2 ? counter = 0 : anotherCounter 
+    if(x.matches){
+      if(counter == 0){
+          containerCards.classList.remove('transition')
+          containerCards.classList.add('transform') 
+        }
+    }
+   
+    setTimeout(() => {
+      if(x.matches){
+          if(counter == 0){
+          containerCards.classList.add('transition')
+          containerCards.classList.remove('transform')
+          textSkills.classList.add('plus-height')
+          counter = 1;
+         }
+      }
+      
+      if (oldCard == card){
+        textSkills.innerHTML = "Choose another skill"
+        
+        if(x.matches && counter == 1){
+          
+            containerCards.classList.remove('transition')
+            containerCards.classList.add('transform-back') 
+            counter = 2
+            setTimeout(() => {
+              containerCards.classList.add('transition')
+              containerCards.classList.remove('transform-back') 
+              textSkills.classList.remove('plus-height') 
+            },1000)
+            }
+       
+      }else{
+        if (card == arrayCards[0]){
+          textSkills.innerHTML = "HTML was the first skill that I learned, I used in a lot of my projects, in one project expecific a learn a lot of HTML in how to create a form. It's not easy to learn about DOM elements and how to integrate them with JS."
+          
+          
+        }
+        if(card == arrayCards[1]){
+          textSkills.innerHTML = "I love CSS so much, It's facinating what we can do with CSS. I try to do a lot of animations and things that I don't use before to learn more and more about CSS."
+          
+          
+        }
+        if(card == arrayCards[2]){
+          textSkills.innerHTML = "I used a lot of SCSS in my projects, it's easiear to create variables with SCSS and to interact with DOM elements. It's so good how you can keep your files organized with SCSS."
+          
+          
+        }
+        if(card == arrayCards[3]){
+          textSkills.innerHTML = "React is new for me, I learned a little in a recent project and I'm trying to use more in everything I do."
+          
+          
+        }
+        if(card == arrayCards[4]){
+          textSkills.innerHTML = "I'm learning Back-end Python in EBAC, using Django, Ngnix, Gunicorn and integrate this in a Virtual Box machine using Linux."
+          
+          
+        }
+        if(card == arrayCards[5]){
+          textSkills.innerHTML = "Part of my course in EBAC teach's SQL to create a database, relations and use the back-end to interact with the data source."
+          
+          
+        }
+      }
+      
+      oldCard = card;
+      project.style.opacity = 1;
+   }, 1000)
+   
   })
 })
